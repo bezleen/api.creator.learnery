5b40149f51f51993211db006d43bf7d8d48aab8b
aldoprogrammer <aldols@bugcrowdninja.com>
2023-08-26 13:52:39 +0700

    fix the e2e

---
+++ jest-config.ts
@@ -0,0 +1,16 @@
+import { pathsToModuleNameMapper } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json' 
+
+const jestConfig = {
+  preset: 'ts-jest',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  testEnvironment: 'node',
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testRegex: '.*\\.spec\\.ts$',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+}
+
+export default jestConfig
