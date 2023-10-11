8610828f58712fad751a081e980f88c4972df757
xuanbachdao0212 <xuanbachdao0212@gmail.com>
2023-10-11 15:35:30 +0700

    disable course schema

--- src/graphql.ts
+++ src/graphql.ts
@@ -41,93 +41,6 @@ export class CreateAudienceInput {
     desc?: Nullable<string>;
 }
 
-export class CreateCourse {
-    description: string;
-    language?: Nullable<string>;
-    category: string;
-    tone: string;
-    modality: string;
-    classSize: string;
-    topics?: Nullable<Nullable<string>[]>;
-    duration: number;
-    durationLesson: number;
-    audience: CreateAudienceInput;
-}
-
-export class UpdateCourse {
-    title?: Nullable<string>;
-    objective?: Nullable<string[]>;
-    description?: Nullable<string>;
-    language?: Nullable<string>;
-    category?: Nullable<string>;
-    tone?: Nullable<string>;
-    modality?: Nullable<string>;
-    classSize?: Nullable<string>;
-    topics?: Nullable<string[]>;
-    duration?: Nullable<number>;
-    durationLesson?: Nullable<number>;
-    audience?: Nullable<CreateAudienceInput>;
-    outline?: Nullable<UpdateCourseOutline>;
-    detailedOutline?: Nullable<UpdateCourseDetailedOutline>;
-}
-
-export class CreateCourseTitle {
-    courseId: string;
-}
-
-export class CreateCourseObjective {
-    courseId: string;
-}
-
-export class CreateCourseOutline {
-    courseId: string;
-}
-
-export class UpdateCourseOutline {
-    courseDuration: string;
-    inScope: InputTopic[];
-    outOfScope: InputTopic[];
-    explanation?: Nullable<string>;
-}
-
-export class UpdateCourseDetailedOutline {
-    sections: UpdateDetailedOutlineSectionInput[];
-    objectiveTopicConnections: UpdateDetailedOutlineObjectiveInput[];
-    summary: CourseDetailedSummaryInput;
-    rationale: string;
-}
-
-export class DetailedOutlineTopicInput {
-    title: string;
-    onScreenContent: string;
-    topicDescription: string;
-    detailedCoverage: string;
-    resources: string;
-    connection: string[];
-    timingMins: number;
-}
-
-export class UpdateDetailedOutlineSectionInput {
-    title: string;
-    topics?: Nullable<DetailedOutlineTopicInput[]>;
-}
-
-export class UpdateDetailedOutlineObjectiveInput {
-    objective: string;
-    topics?: Nullable<string[]>;
-}
-
-export class CourseDetailedSummaryInput {
-    totalSections: number;
-    totalTopics: number;
-    courseHours: number;
-}
-
-export class InputTopic {
-    topic: string;
-    subtopics: Nullable<string>[];
-}
-
 export class CreatePerformanceTaskInput {
     objectives: string;
     description?: Nullable<string>;
@@ -194,20 +107,6 @@ export class AuthPayload {
 export abstract class IMutation {
     abstract signIn(data: AuthInput): AuthPayload | Promise<AuthPayload>;
 
-    abstract createCourse(data: CreateCourse): Nullable<Course> | Promise<Nullable<Course>>;
-
-    abstract updateCourse(id: string, data: UpdateCourse): Course | Promise<Course>;
-
-    abstract deleteCourse(id: string): Nullable<Course> | Promise<Nullable<Course>>;
-
-    abstract createCourseTitle(courseId: string): Nullable<GeneratedCourseTitle>[] | Promise<Nullable<GeneratedCourseTitle>[]>;
-
-    abstract createCourseObjective(courseId: string): Nullable<GeneratedCourseObjective>[] | Promise<Nullable<GeneratedCourseObjective>[]>;
-
-    abstract createCourseOutline(courseId: string): CourseOutline | Promise<CourseOutline>;
-
-    abstract createCourseDetailedOutline(courseId: string): CourseDetailedOutline | Promise<CourseDetailedOutline>;
-
     abstract createPerformanceTask(data: CreatePerformanceTaskInput): Nullable<RespondPerformanceTask> | Promise<Nullable<RespondPerformanceTask>>;
 
     abstract removePerformanceTask(id: string): Nullable<RespondPerformanceTask> | Promise<Nullable<RespondPerformanceTask>>;
@@ -224,10 +123,6 @@ export abstract class IMutation {
 export abstract class IQuery {
     abstract categories(): string[] | Promise<string[]>;
 
-    abstract courses(): Nullable<Course>[] | Promise<Nullable<Course>[]>;
-
-    abstract course(id: string): Course | Promise<Course>;
-
     abstract performanceTasks(): Nullable<RespondPerformanceTask>[] | Promise<Nullable<RespondPerformanceTask>[]>;
 
     abstract performanceTask(id: string): RespondPerformanceTask | Promise<RespondPerformanceTask>;
@@ -243,24 +138,12 @@ export abstract class IQuery {
     abstract worksheet(id: string): RespondWorksheet | Promise<RespondWorksheet>;
 }
 
-export class Course {
-    id?: Nullable<string>;
-    title?: Nullable<string>;
-    objective?: Nullable<string[]>;
-    description: string;
-    language?: Nullable<string>;
-    category: string;
+export class PerformanceTask {
+    objectives: string;
+    description?: Nullable<string>;
     tone: string;
     modality: string;
-    classSize: string;
-    topics?: Nullable<string[]>;
-    duration: number;
-    durationLesson: number;
-    creatorId: string;
-    outline?: Nullable<CourseOutline>;
-    detailedOutline?: Nullable<CourseDetailedOutline>;
-    createdAt: DateTime;
-    updatedAt: DateTime;
+    language?: Nullable<string>;
     audience: Audience;
 }
 
@@ -269,74 +152,6 @@ export class Audience {
     ageEnd: number;
     level: string;
     desc?: Nullable<string>;
-}
-
-export class GeneratedCourseTitle {
-    title: string;
-    reason?: Nullable<string>;
-}
-
-export class GeneratedCourseObjective {
-    objective: string;
-    outcome?: Nullable<string>;
-}
-
-export class CourseOutline {
-    courseDuration: string;
-    inScope: Topic[];
-    outOfScope: Topic[];
-    explanation?: Nullable<string>;
-}
-
-export class CourseDetailedOutline {
-    sections: DetailedOutlineSection[];
-    objectiveTopicConnections: DetailedOutlineObjective[];
-    summary: CourseDetailedSummary;
-    rationale: string;
-}
-
-export class DetailedOutlineSection {
-    title: string;
-    topics: Nullable<DetailedOutlineTopic>[];
-}
-
-export class DetailedOutlineTopic {
-    title: string;
-    onScreenContent: string;
-    topicDescription: string;
-    detailedCoverage: string;
-    resources: string;
-    connection: string[];
-    timingMins: number;
-}
-
-export class DetailedOutlineObjective {
-    objective: string;
-    topics?: Nullable<string[]>;
-}
-
-export class CourseDetailedSummary {
-    totalSections: number;
-    totalTopics: number;
-    courseHours: number;
-}
-
-export class Topic {
-    topic: string;
-    subtopics: string[];
-}
-
-export abstract class ISubscription {
-    abstract courseUpdated(courseId: string): Course | Promise<Course>;
-}
-
-export class PerformanceTask {
-    objectives: string;
-    description?: Nullable<string>;
-    tone: string;
-    modality: string;
-    language?: Nullable<string>;
-    audience: Audience;
 }
 
 export class RespondPerformanceTask {
