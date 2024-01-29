b696b623c9a0a5293cff511534ef2eaa55336536
xuanbachdao0212 <xuanbachdao0212@gmail.com>
2024-01-29 23:52:16 +0700

    Remove unused modules

--- src/course/course.module.ts
+++
@@ -1,9 +0,0 @@
-// import { Module } from '@nestjs/common'
-// import { CourseService } from './course.service'
-// import { CourseResolver } from './course.resolver'
-// import { OpenAIService } from '@ai/openai/openAIService'
-
-// @Module({
-//   providers: [CourseResolver, CourseService, OpenAIService],
-// })
-// export class CourseModule {}
